a11d0580dfdf6d1f97649acb59331b12bb3c0977
Afifah nuha <afnuahamed723@gmail.com>
2025-10-13 18:48:36 +0530

    Time: 1 ms (99.87%) | Memory: 49.4 MB (9.65%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pointerA = headA; // Initialize pointer A to the head of list A. It's one of our runners.
+        ListNode pointerB = headB; // Initialize pointer B to the head of list B. This is our second runner.
+
+        while (pointerA != pointerB) { // Keep going until our runners meet! Or until we're absolutely sure they won't meet.
+            if (pointerA == null) { // If runner A reaches the end of its track...
+                pointerA = headB; // ...it starts running on track B! It's like saying "Okay, let's see if we can meet if I run the other way".
+            } else {
+                pointerA = pointerA.next; // Otherwise, runner A continues to the next node on its track. Just keep running!
+            }
+
+            if (pointerB == null) { // Same logic as above, but for runner B. If runner B reaches the end of its track...
+                pointerB = headA; // ...it starts running on track A!
+            } else {
+                pointerB = pointerB.next; // Otherwise, runner B continues to the next node on its track.
+            }
+        }
+
+        return pointerA; // If the runners meet (pointerA == pointerB), we've found the intersection! Return that node. If the lists don't intersect at all, both runners will be null, and we return null.
+    }
+}
